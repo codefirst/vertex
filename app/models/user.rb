85b96ec425cb1df29e7570af66d80bd77c28a434
mallowlabs <mallowlabs@gmail.com>
2014-11-13 20:55:16 +0900

    Changed: task count check

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ActiveRecord::Base
   has_many :tasks
 
   def notify!
-    Task.where("user_id = ?", self.id).update_all(done: false)
-    content = Vertex::Taskpaper.new.report(self, self.tasks)
-    Vertex::AsakusaSatellite.new.notify(content)
+    tasks = Task.where("user_id = ?", self.id)
+    unless tasks.empty?
+      tasks.update_all(done: false)
+      content = Vertex::Taskpaper.new.report(self, self.tasks)
+      Vertex::AsakusaSatellite.new.notify(content)
+    end
   end
 end
