8bd2fc430d96d884228cbfcc16ac57ff6090e0fd
Ryo SUETSUGU <suetsugu.r@gmail.com>
2019-09-08 22:40:30 +0900

    Fixed: error when not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+
   acts_as_token_authentication_handler_for User, if: lambda { |controller| controller.request.format.json? }
 
   def verify_authenticity_token
